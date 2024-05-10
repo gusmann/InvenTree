db689383acb7386f3c4f52d1e30fcd76d1e66278
Gustavo Argote <gusmann@users.noreply.github.com>
2024-05-10 22:53:14 +0000

    build: Add helm & kubectl to devcontainer
    Helm CLI & kubectl will now be installed in devcontainer image for chart/k8s development

--- contrib/container/install_build_packages.sh
+++ contrib/container/install_build_packages.sh
@@ -8,5 +8,23 @@ apk add gcc g++ musl-dev openssl-dev libffi-dev cargo python3-dev openldap-dev \
     jpeg-dev openjpeg-dev libwebp-dev zlib-dev \
     sqlite sqlite-dev \
     mariadb-connector-c-dev mariadb-client mariadb-dev \
-    postgresql13-dev postgresql-libs \
+    postgresql13-dev postgresql-libs yq curl \
     $@
+
+
+install helm & kubectl
+case `uname -m` in \
+        x86_64) ARCH=amd64; ;; \
+        armv7l) ARCH=arm; ;; \
+        aarch64) ARCH=arm64; ;; \
+        ppc64le) ARCH=ppc64le; ;; \
+        s390x) ARCH=s390x; ;; \
+        *) echo "un-supported arch, exit ..."; exit 1; ;; \
+esac && \
+curl -sL https://get.helm.sh/helm-v${HELM_VERSION}-linux-${ARCH}.tar.gz | tar -xvz && \
+mv linux-${ARCH}/helm /usr/bin/helm && \
+chmod +x /usr/bin/helm && \
+rm -rf linux-${ARCH} && \
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl" && \
+mv kubectl /usr/bin/kubectl && \
+chmod +x /usr/bin/kubectl
